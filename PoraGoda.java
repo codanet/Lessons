480bf251c00b33b6ba74d609823a62f28cc372f2
codanet <ttmts@ua.fm>
2016-03-24 23:18:24 +0200

    Added files via upload

---
+++ PoraGoda.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class PoraGoda {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("vvedite 4islo");
+        int a = in.nextInt();//считываем целое число a
+        switch (a) {
+            case 1:
+                System.out.println("Zima");
+                break;
+            case 2:
+                System.out.println("Zima");
+                break;
+            case 12:
+                System.out.println("Zima");
+                break;
+            case 3:
+                System.out.println("vesna");
+                break;
+            case 4:
+                System.out.println("vesna");
+                break;
+            case 5:
+                System.out.println("vesna");
+                break;
+            case 6:
+                System.out.println("leto");
+                break;
+            case 7:
+                System.out.println("leto");
+                break;
+            case 8:
+                System.out.println("leto");
+                break;
+            case 9:
+                System.out.println("osen'");
+                break;
+            case 10:
+                System.out.println("osen'");
+                break;
+            case 11:
+                System.out.println("osen'");
+                break;
+
+
+        }
+    }
+}
